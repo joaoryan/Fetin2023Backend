13c6ca0ebc1c8c3cbdc86403c35817c485620eb4
João Ryan Santos <sjoaoryan@gmail.com>
2023-08-15 21:41:22 -0300

    chore: alteraçoes codigo

--- app.js
+++ app.js
@@ -18,6 +18,19 @@ const connection = mysql.createConnection({
     database: 'saveFace', // Nome do banco de dados
 });
 
+app.get("/login/:email/:password", async (req, res) => {
+    const sqlQuery = `SELECT * FROM users where email = ${req.params.email} AND typeUser ='visitant'`;
+
+    connection.query(sqlQuery, (err, results) => {
+        if (err) {
+            console.error('Erro na consulta SQL:', err);
+            res.status(500).json({ error: 'Erro ao consultar o banco de dados.' });
+        } else {
+            res.json(results);
+        }
+    })
+})
+
 app.get("/visitant/:id", async (req, res) => {
     const sqlQuery = `SELECT * FROM users where residentId = ${req.params.id} AND typeUser ='visitant'`;
 
@@ -44,7 +57,7 @@ app.get("/image/:serialNumber", async (req, res) => {
     })
 })
 
-app.delete("/visitant/:id", async (req, res) => {
+app.delete("/user/:id", async (req, res) => {
     const sqlQuery = `DELETE FROM users WHERE id = ${req.params.id};`;
     connection.query(sqlQuery, (err, results) => {
         if (err) {
@@ -63,42 +76,39 @@ app.post("/visitant", async (req, res) => {
             console.error('Erro na consulta SQL:', err);
             res.status(500).json({ error: 'Erro ao consultar o banco de dados.' });
         } else {
-            req.body.image.map((value) => {
+            res.json(results);
+        }
+    })
+})
 
-                /* const sqlQuery = `INSERT INTO image (userId, image, serialNumber) VALUES (${results.insertId}, "${value}", "${req.body.serialNumber}");`;
-                 connection.query(sqlQuery, (err, results) => {
-                     if (err) {
-                         console.error('Erro na consulta SQL:', err);
-                         res.status(500).json({ error: 'Erro ao consultar o banco de dados.' });
-                     } else {
-                     }
-                 })
-                 */
-            })
+app.post("/img", async (req, res) => {
+    const sqlQuery = `INSERT INTO image (userId, image, serialNumber) VALUES (${req.body.userId}, "${req.body.image}", "${req.body.serialNumber}");`;
+    connection.query(sqlQuery, (err, results) => {
+        if (err) {
+            console.error('Erro na consulta SQL:', err);
+            res.status(500).json({ error: 'Erro ao consultar o banco de dados.' });
+        } else {
             res.json(results);
         }
     })
 })
 
 app.post("/user", async (req, res) => {
-
-
-
-    const sqlQuery = `INSERT INTO users (residentId, name, phone, typeUser) VALUES (${req.body.residentId}, "${req.body.name}", "${req.body.phone}", "visitant");`;
-    connection.query(sqlQuery, (err, results) => {
+    const sqlQueryResident = `INSERT INTO users (email, address) VALUES ("${req.body.email}", "${req.body.address}");`;
+    connection.query(sqlQueryResident, (err, results) => {
         if (err) {
             console.error('Erro na consulta SQL:', err);
             res.status(500).json({ error: 'Erro ao consultar o banco de dados.' });
         } else {
-            req.body.image.map((value) => {
-                const sqlQuery = `INSERT INTO image (userId, image, serialNumber) VALUES (${results.insertId}, "${value}", "${req.body.serialNumber}");`;
-                connection.query(sqlQuery, (err, results) => {
-                    if (err) {
-                        console.error('Erro na consulta SQL:', err);
-                        res.status(500).json({ error: 'Erro ao consultar o banco de dados.' });
-                    } else {
-                    }
-                })
+            //res.json(results);
+            const sqlQueryUser = `INSERT INTO users (residentId, name, phone, typeUser, image) VALUES (${results.data.id}, "${req.body.name}", "${req.body.phone}", "visitant", "${req.body.image}");`;
+            connection.query(sqlQueryUser, (err, results) => {
+                if (err) {
+                    console.error('Erro na consulta SQL:', err);
+                    res.status(500).json({ error: 'Erro ao consultar o banco de dados.' });
+                } else {
+                    res.json(results);
+                }
             })
         }
     })
